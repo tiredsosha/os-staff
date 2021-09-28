6e9f9b016bba89ff1d12cf0ac99fb5acb2b814d6
Чичко Александра Алексеевна <aachichko@sberautotech.ru>
2021-09-28 23:35:45 +0300

    f

---
+++ win10/quick_setup/winconfig/winconfig.bat
@@ -0,0 +1,14 @@
+LGPO.exe /g c:\app\winconfig
+gpupdate /force
+NetSh Advfirewall set allprofiles state off
+powercfg /setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
+powercfg /SETDCVALUEINDEX SCHEME_CURRENT 7516b95f-f776-4464-8c53-06167f40cc99 3c0bc021-c8a8-4e07-a973-6b14cbcb2b7e 0
+powercfg /SETACVALUEINDEX SCHEME_CURRENT 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
+powercfg /SETDCVALUEINDEX SCHEME_CURRENT 0012ee47-9041-4b5d-9b77-535fba8b1442 6738e2c4-e8a5-4a42-b16a-e040e769756e 0
+powershell -command "&{$p='HKCU:SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StuckRects3';$v=(Get-ItemProperty -Path $p).Settings;$v[8]=3;&Set-ItemProperty -Path $p -Name Settings -Value $v;&Stop-Process -f -ProcessName explorer}"
+taskkill /im MicrosoftEdgeUpdate.exe /f
+rd "%ProgramFiles(x86)%\Microsoft\EdgeUpdate" /s /q
+powercfg -h off
+regchanger.reg
+shutdown /r
